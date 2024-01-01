fedbf527b7c50856ae095cbd8b978f630ee9700e
4121n <4121nnn@gmail.com>
2024-01-01 22:36:48 +0300

    top bar and exit button

--- src/main/java/minesweeper/Controller.java
+++ src/main/java/minesweeper/Controller.java
@@ -1,8 +1,11 @@
 package minesweeper;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Controller {
     @FXML
@@ -12,12 +15,25 @@ public class Controller {
 
     public void initialize(){
         UI UI = new UI(root, gridPane);
-        Engine engine = new Engine(UI,10);
+        Engine engine = new Engine(UI,35);
         UI.setEngine(engine);
         UI.createGrid(engine.board.cells, UI.getRow(), UI.getCol());
 
     }
+    double xOffset, yOffset;
+
+    @FXML
+    private void handleMousePressed(MouseEvent event) {
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+    }
+    @FXML
+    private void handleMouseDragged(MouseEvent event) {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
+    }
 
 
-
+    public void exit() { System.exit(0); }
 }
--- src/main/java/minesweeper/UI.java
+++ src/main/java/minesweeper/UI.java
@@ -2,25 +2,56 @@ package minesweeper;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class UI {
     Pane root;
     GridPane gridPane;
     Engine engine;
-    private int width = 800, height = 500, topBarHeight = 25, gridPaneHeight = height - topBarHeight, row = 10, col = 10;
+
+    Pane topBar;
+    Pane main;
+    private int width = 800, height = 500, topBarHeight = 35,  row = 10, col = 10, padding = 20;
     public UI(Pane root, GridPane gridPane){
+        this.root = root;
         root.setMinWidth(width);
         root.setMinHeight(height);
         gridPane.setLayoutY(topBarHeight);
         this.gridPane = gridPane;
+        this.topBar = (Pane) root.lookup("#topBar");
+        this.main = (Pane) root.lookup("#main");
+        setTopMenu();
+        setMain();
 
     }
 
+    public void setTopMenu(){
+        topBar.setMinWidth(width);
+        topBar.setMinHeight(topBarHeight);
+        Button exitButton = (Button) topBar.lookup("#exitButton");
+        exitButton.setMinWidth(topBarHeight);
+        exitButton.setMinHeight(topBarHeight);
+        exitButton.setLayoutX(width - topBarHeight);
+    }
+    public void setMain(){
+        main.setMinHeight(height - topBarHeight);
+        main.setMinWidth(width);
+        main.setMaxHeight(height - topBarHeight);
+        main.setLayoutY(topBarHeight);
+
+    }
+
+
+
+
+
     public void createGrid(Cell[][] cells, int row, int col){
-        int btnSize = gridPaneHeight  / row;
+        int btnSize =  (height - topBarHeight-padding*2)/ row;
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 Button btn = createButton(i, j, btnSize);
@@ -30,7 +61,7 @@ public class UI {
         }
 
         gridPane.setLayoutX((width - btnSize * col) / 2);
-        gridPane.setLayoutY(topBarHeight);
+        gridPane.setLayoutY(topBarHeight- padding);
 
     }
     public Button createButton(int row, int col, int size){
