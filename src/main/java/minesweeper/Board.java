3e17aea45bc2a57be26c023f19dbcbdf4cd53235
4121n <4121nnn@gmail.com>
2024-01-01 20:16:20 +0300

    simpe version of minesweeper

--- src/main/java/minesweeper/Board.java
+++ src/main/java/minesweeper/Board.java
@@ -1,13 +1,8 @@
 package minesweeper;
 
 public class Board {
-    private int row, col;
-    int mines;
     Cell[][] cells;
     public Board(int row, int col , int mines){
-        this.row = row;
-        this.col = col;
-        this.mines = mines;
         cells = new Cell[row][col];
 
         for(int i = 0; i < row; i++){
